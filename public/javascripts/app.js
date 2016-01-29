187e71edbd17108915306e396bdd72e04fd6fbe6
Ash Kestrel <tetrapteryxgames@gmail.com>
2016-01-29 10:17:08 -0700

    FILES

---
+++ public/javascripts/app.js
@@ -0,0 +1,54 @@
+var playerId;
+
+$(function() {
+  var socket = io();
+});
+
+socket.on('connect', function(player_id) {
+  playerId = player_id;
+});
+
+socket.on('pair', function(role){
+  if (role === 1) {
+    generateGame('safe');
+  } else {
+    generateGame('guide');
+  }
+});
+
+socket.on('chat message', function(message) {
+
+});
+
+function generateGame(type) {
+  var Game = React.createClass({
+    getInitialState: function(){
+      return {player_type: type};
+    },
+    handleChange: function(event) {
+
+    },
+    render: function() {
+      if (this.state.type === 'safe') {
+        return <div class="safe">
+
+        </div>;
+      } else {
+        return <div class="guide">
+
+        </div>;
+      }
+    }
+  });
+
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('game');
+  );
+
+  gameLoop();
+}
+
+function gameLoop() {
+
+}
